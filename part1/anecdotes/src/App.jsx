020f0cb85d8884ab24ebd37c990dd42c4bc3f1d1
Henry Gao <hgao080@aucklanduni.ac.nz>
2024-11-27 16:26:04 +1300

    Added showing anecdote with most votes

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -23,12 +23,15 @@ const App = () => {
 
   return (
     <div>
+      <h1>Anecdote of the day</h1>
       {anecdotes[selected]}
       <p>has {points[selected]} votes</p>
       <div>
         <button onClick={handleVote}>vote</button>
         <button onClick={() => {setSelected(Math.floor(Math.random() * 8))}}>next anecdote</button>
       </div>
+      <h1>Anecdote with most votes</h1>
+      {Math.max(...points) == 0 ? 'No anecdotes have been voted for' : anecdotes[points.indexOf(Math.max(...points))]}
     </div>
   )
 }
