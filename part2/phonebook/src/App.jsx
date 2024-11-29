416cc6d0c2ba578bf0da7786bbab354fc3af8954
Henry Gao <hgao080@aucklanduni.ac.nz>
2024-11-29 13:33:22 +1300

    Added function to add names to phonebook

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+
+import Details from './components/Details'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const addName = (e) => {
+    e.preventDefault()
+    const nameObject = {
+      name: newName
+    }
+
+    setPersons(persons.concat(nameObject))
+    setNewName('')
+  }
+
+  const handleNameChange = (e) => {
+    setNewName(e.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person) => (
+        <Details key={person.name} person={person}></Details>
+      ))}
+    </div>
+  )
+}
+
+export default App
---
+++ part2/phonebook/src/components/Details.jsx
@@ -0,0 +1,7 @@
+const Details = ({person}) => {
+    return (
+        <p><b>{person.name}</b></p>
+    );
+}
+ 
+export default Details;
