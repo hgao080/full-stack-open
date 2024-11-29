8f2623293e417ca7c3073832bc871ab8d1411876
Henry Gao <hgao080@aucklanduni.ac.nz>
2024-11-29 14:07:45 +1300

    Refactired by extracting suitable components

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,73 +1,70 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import Details from './components/Details'
+import Details from "./components/Details";
+import Filter from "./components/Filter";
+import PersonForm from "./components/PersonForm";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  ]);
 
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
-  const [filter, setFilter] = useState('')
-  const personsToShow = filter.length === 0 ? persons : persons.filter(person => person.name.toLowerCase().startsWith(filter.toLowerCase()))
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [filter, setFilter] = useState("");
+  const personsToShow =
+    filter.length === 0
+      ? persons
+      : persons.filter((person) =>
+          person.name.toLowerCase().startsWith(filter.toLowerCase())
+        );
 
   const addPerson = (e) => {
-    e.preventDefault()
-    
-    if (persons.find(person => person.name === newName)) {
-      alert(`${newName} is already added to phonebook`)
-      return
+    e.preventDefault();
+
+    if (persons.find((person) => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`);
+      return;
     }
 
     const nameObject = {
       name: newName,
       number: newNumber,
       id: String(persons.length + 1),
-    }
+    };
 
-    setPersons(persons.concat(nameObject))
-    setNewName('')
-    setNewNumber('')
-  }
+    setPersons(persons.concat(nameObject));
+    setNewName("");
+    setNewNumber("");
+  };
 
   const handleNameChange = (e) => {
-    setNewName(e.target.value)
-  }
+    setNewName(e.target.value);
+  };
 
   const handleNumberChange = (e) => {
-    setNewNumber(e.target.value)
-  }
+    setNewNumber(e.target.value);
+  };
 
   const handleFilterChange = (e) => {
-    setFilter(e.target.value)
-  }
+    setFilter(e.target.value);
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>
-        filter shown with <input value={filter} onChange={handleFilterChange} />
-      </div>
-      <form onSubmit={addPerson}>
-        <h1>add a new</h1>
-        <div>
-          name: <input value={newName} onChange={handleNameChange}/>
-        </div>
-        <div>number: <input value={newNumber} onChange={handleNumberChange} /></div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <Filter filter={filter} handleFilterChange={handleFilterChange}></Filter>
+      <h3>add a new</h3>
+      <PersonForm newName={newName} handleNameChange={handleNameChange} newNumber={newNumber} handleNumberChange={handleNumberChange} addPerson={addPerson}></PersonForm>
       <h2>Numbers</h2>
       {personsToShow.map((person) => (
         <Details key={person.id} person={person}></Details>
       ))}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ part2/phonebook/src/components/PersonForm.jsx
@@ -0,0 +1,19 @@
+const PersonForm = (props) => {
+  const { newName, handleNameChange, newNumber, handleNumberChange, addPerson } = props;
+
+  return (
+    <form onSubmit={addPerson}>
+      <div>
+        name: <input value={newName} onChange={handleNameChange} />
+      </div>
+      <div>
+        number: <input value={newNumber} onChange={handleNumberChange} />
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  );
+};
+
+export default PersonForm;
